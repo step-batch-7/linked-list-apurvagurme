0377e70614e6145a658bc6bd251466d2f5b38c6e
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-05-06 23:24:00 +0530

    Added assert function

--- test/test.c
+++ test/test.c
@@ -2,35 +2,52 @@
 #include<stdlib.h>
 #include"../list.h"
 
-void print_test_result(List_ptr list, char msg[], int result, int count, int head_value, int last_value)
+char *assert(List_ptr list, char msg[], int result, int count, int expected_list_values[])
 {
-  char *sign = "❎ ";
-  if (result == Success && list->count == count && list->head->value == head_value && list->last->value == last_value)
+  char *sign = "✅ ";
+  int counter = 0;
+  if (result == Success && list->count == count)
   {
-    sign = "✅ ";
+    Node_ptr p_walk = list->head;
+    while (p_walk != NULL)
+    {
+      if (p_walk->value != expected_list_values[counter])
+      {
+        sign = "❎ ";
+        break;
+      }
+      counter++;
+      p_walk = p_walk->next;
+    }
   }
-  printf("%s %s", sign, msg);
+  return sign;
 }
 
 void add_to_end_in_empty_list(List_ptr list)
 {
   int result = add_to_end(list, 1);
   char msg[] = "Should add add to the end of empty list\n";
-  print_test_result(list, msg, result, 1, 1, 1);
+  int expected_values[1] = {1};
+  char *sign = assert(list, msg, result, 1, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void add_to_end_in_list_of_1_number(List_ptr list)
 {
   int result = add_to_end(list, 2);
   char msg[] = "Should add to the end of list having one number\n";
-  print_test_result(list, msg, result, 2, 1, 2);
+  int expected_values[2] = {1, 2};
+  char *sign = assert(list, msg, result, 2, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void add_to_end_in_list_of_having_more_than_one_numbers(List_ptr list)
 {
   int result = add_to_end(list, 3);
   char msg[] = "Should add add to the end of list having more than one number\n";
-  print_test_result(list, msg, result, 3, 1, 3);
+  int expected_values[3] = {1, 2, 3};
+  char *sign = assert(list, msg, result, 3, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void test_add_to_end(void)
@@ -45,21 +62,27 @@ void add_to_start_in_empty_list(List_ptr list)
 {
   int result = add_to_start(list, 1);
   char msg[] = "Should add add to the start of empty list\n";
-  print_test_result(list, msg, result, 1, 1, 1);
+  int expected_values[1] = {1};
+  char *sign = assert(list, msg, result, 1, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void add_to_start_in_list_of_1_number(List_ptr list)
 {
   int result = add_to_start(list, 2);
   char msg[] = "Should add to the start of list having one number\n";
-  print_test_result(list, msg, result, 2, 2, 1);
+  int expected_values[2] = {2, 1};
+  char *sign = assert(list, msg, result, 2, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void add_to_start_in_list_of_having_more_than_one_numbers(List_ptr list)
 {
   int result = add_to_start(list, 3);
   char msg[] = "Should add add to the start of list having more than one number\n";
-  print_test_result(list, msg, result, 3, 3, 1);
+  int expected_values[3] = {3, 2, 1};
+  char *sign = assert(list, msg, result, 3, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void test_add_to_start(void)
@@ -74,28 +97,36 @@ void insert_at_the_0th_position_of_empty_list(List_ptr list)
 {
   int result = insert_at(list, 0, 0);
   char msg[] = "Should insert at the 0th position of an empty list\n";
-  print_test_result(list, msg, result, 1, 0, 0);
+  int expected_values[1] = {0};
+  char *sign = assert(list, msg, result, 1, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void insert_at_the_0th_position_of_list_having_one_element(List_ptr list)
 {
   int result = insert_at(list, 1, 0);
   char msg[] = "Should insert at the 0th position of list having one element\n";
-  print_test_result(list, msg, result, 2, 1, 0);
+  int expected_values[2] = {1, 0};
+  char *sign = assert(list, msg, result, 2, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void insert_at_the_1st_position_or_middle_of_list_having_two_element(List_ptr list)
 {
   int result = insert_at(list, 2, 1);
   char msg[] = "Should insert at the 1st position of list having two element\n";
-  print_test_result(list, msg, result, 3, 1, 0);
+  int expected_values[3] = {1, 2, 0};
+  char *sign = assert(list, msg, result, 3, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void insert_at_the_last_position_of_list(List_ptr list)
 {
   int result = insert_at(list, 4, list->count);
   char msg[] = "Should insert at the last position of list\n";
-  print_test_result(list, msg, result, 4, 1, 4);
+  int expected_values[4] = {1, 2, 0, 4};
+  char *sign = assert(list, msg, result, 4, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void test_insert_at(void)
@@ -111,7 +142,9 @@ void add_unique_if_list_is_empty(List_ptr list)
 {
   int result = add_unique(list, 0);
   char msg[] = "Should add at end of an empty list\n";
-  print_test_result(list, msg, result, 1, 0, 0);
+  int expected_values[1] = {0};
+  char *sign = assert(list, msg, result, 1, expected_values);
+  printf("%s %s", sign, msg);
 }
 
 void test_add_unique(void)
